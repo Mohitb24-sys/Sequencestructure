ddfda38bc1d6e550ce68870031ca258c124c5a4c
mohit bharwani <padi.ashok2007@gmail.com>
2025-09-01 23:52:59 +0530

    Create mb1.c

---
+++ mb1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("ENTER TWO NUMBERS:");
+scanf("%d%d",&a,&b);
+if(a>=b)
+{
+printf("a is largest",a);
+}
+else
+{
+printf("b is largest",b);
+}
+printf("FOR SMALLEST AMONG TWO:");
+{
+if(a<=b)
+{
+printf("a is smallest",a);
+}
+else
+{
+printf("b is smallest",b);
+}
+getch();
+}
